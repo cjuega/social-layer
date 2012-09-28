5a9ee2c1c7b21fad7034b7b93c5f8e556787745f
Carlos Juega <carlosjuegasi@gmail.com>
2012-09-28 15:16:52 +0200

    Add a StaticPages controller

--- db/migrate/20120928125414_create_messages.rb
+++ db/migrate/20120928125414_create_messages.rb
@@ -1,7 +1,7 @@
 class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
-      t.string :content
+      t.text :content
       t.integer :user_id
       t.integer :privacity_id
 
