78066d5a12f2e4bdb5b02fda2eee1bfc9332560c
Carlos Juega <carlosjuegasi@gmail.com>
2012-09-28 17:50:08 +0200

    Make a basic User model (including secure passwords)

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id           :integer          not null, primary key
+#  content      :text
+#  user_id      :integer
+#  privacity_id :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Message < ActiveRecord::Base
   attr_accessible :content, :privacity_id, :user_id
   
--- test/unit/message_test.rb
+++ test/unit/message_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id           :integer          not null, primary key
+#  content      :text
+#  user_id      :integer
+#  privacity_id :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 require 'test_helper'
 
 class MessageTest < ActiveSupport::TestCase
